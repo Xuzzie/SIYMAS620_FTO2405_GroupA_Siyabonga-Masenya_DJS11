39d1da85f74f02151c4df1809bcaed95df1e8681
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-15 09:38:17 +0200

    I set up the react enviroment and cleaned up the boilerplate for app js

---
+++ DJS11/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Podcast App</h1>
+      <p>Welcome to your Podcast App!</p>
+    </div>
+  );
+}
+
+export default App;
