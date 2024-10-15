42de31c57cb95561bc7538e22d645fa5aba1c460
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-15 10:41:24 +0200

    imported useeffect hook to app js

--- DJS11/src/App.jsx
+++ DJS11/src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ShowList from "./components/ShowList";
 function App() {
   return (
     <div>
       <h1>Podcast App</h1>
+      <p>Welcome to the Podcast App!</p>
       <ShowList />
     </div>
   );
